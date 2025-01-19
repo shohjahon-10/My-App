d7c63e48421df9f16c60aa021eaf54901babbc9a
Shohjahon Ro'ziyev <otajonrahmat@gmail.com>
2025-01-19 22:12:29 +0500

    Add videos similar film

--- src/pages/UniqInfo.jsx
+++ src/pages/UniqInfo.jsx
@@ -1,27 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./uniqInfo.css";
+import { Box, Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Controller,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export function UniqInfo() {
   const [getUniqInfo, setGetUniqInfo] = useState({});
-  const [getSimilarFromInfo, setGetSimilarFormInfo] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [allVideos, setAllVideos] = useState([]);
+  const [similarAllFilm, setSimilarAllFilm] = useState([]);
   const { filmID } = useParams();
+  const navigate = useNavigate();
   let newNumbers = [];
-  filmID
-    .split("")
-    .forEach((e) =>
-      String(Number(e)) === "NaN" ? Number(e) : newNumbers.push(e)
-    );
-  console.log(newNumbers.join(""));
+  filmID.split("").forEach((e) => {
+    if (!isNaN(Number(e))) {
+      newNumbers.push(e);
+    }
+  });
 
   const options = {
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGE0OGYyNGNlMWRjZmU3YTI2YTA1YmU3YTNhYmEzZSIsIm5iZiI6MTcwNzExMjEyNi43NzMsInN1YiI6IjY1YzA3NmJlNDM5OTliMDE4NGM5ODllOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._uGk-QvvbXMxib7CyStOVycDVbZ4Zhg_74K2PkCsXMQ",
+      Authorization: "Bearer YOUR_TOKEN_HERE",
     },
   };
 
@@ -38,15 +51,28 @@ export function UniqInfo() {
       setLoading(false);
     }
   };
-  const getSimilarInfo = async () => {
+
+  const gitSimilarVideos = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/${newNumbers.join("")}/videos`,
+        options
+      );
+      setAllVideos(data.results);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+
+  const getSimilar = async () => {
     try {
       const { data } = await axios.get(
         `https://api.themoviedb.org/3/movie/${newNumbers.join("")}/similar`,
         options
       );
-      setGetSimilarFormInfo(data);
-      console.log(data);
-
+      setSimilarAllFilm(data.results);
       setLoading(false);
     } catch (err) {
       setError(err.message);
@@ -56,9 +82,9 @@ export function UniqInfo() {
 
   useEffect(() => {
     getUniqInfoFormApi();
-    getSimilarInfo();
+    gitSimilarVideos();
+    getSimilar();
   }, [filmID]);
-  console.log(getSimilarFromInfo);
 
   if (loading) {
     return <div className="loading-message">Yuklanmoqda...</div>;
@@ -69,28 +95,124 @@ export function UniqInfo() {
   }
 
   return (
-    <div className="container">
-      <h2 className="title">{getUniqInfo.original_title}</h2>
-      <p className="info">
-        <strong>Nomi:</strong> {getUniqInfo.title}
-      </p>
-      <p className="info">
-        <strong>Tavsifi:</strong> {getUniqInfo.overview}
-      </p>
-      <p className="info">
-        <strong>Chiqarilgan sana:</strong> {getUniqInfo.release_date}
-      </p>
-      <p className="info">
-        <strong>Reyting:</strong> {getUniqInfo.vote_average}
-      </p>
-      <p className="info">
-        <strong>Ovoz beruvchilar soni:</strong> {getUniqInfo.vote_count}
-      </p>
-      <img
-        className="poster"
-        src={`https://image.tmdb.org/t/p/w500${getUniqInfo.poster_path}`}
-        alt={getUniqInfo.title}
-      />
-    </div>
+    <>
+      <div className="container">
+        <h2 className="title">{getUniqInfo.original_title}</h2>
+        <p className="info">
+          <strong>Nomi:</strong> {getUniqInfo.title}
+        </p>
+        <p className="info">
+          <strong>Tavsifi:</strong> {getUniqInfo.overview}
+        </p>
+        <p className="info">
+          <strong>Chiqarilgan sana:</strong> {getUniqInfo.release_date}
+        </p>
+        <p className="info">
+          <strong>Reyting:</strong> {getUniqInfo.vote_average}
+        </p>
+        <p className="info">
+          <strong>Ovoz beruvchilar soni:</strong> {getUniqInfo.vote_count}
+        </p>
+        <img
+          className="poster"
+          src={`https://image.tmdb.org/t/p/w500${getUniqInfo.poster_path}`}
+          alt={getUniqInfo.title}
+        />
+      </div>
+      <Box>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y, Controller]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {allVideos?.map((e) => (
+            <SwiperSlide key={e?.key}>
+              <VStack
+                className="film-card"
+                spacing={4}
+                p={4}
+                border="1px solid"
+                borderColor="gray.200"
+                borderRadius="md"
+                boxShadow="md"
+                transition="transform 0.3s"
+                _hover={{
+                  transform: "scale(1.05)",
+                  boxShadow: "lg",
+                  cursor: "pointer",
+                }}
+              >
+                <iframe
+                  style={{ width: "100%", height: "315px" }}
+                  src={`https://www.youtube.com/embed/${e?.key}`}
+                  title={e?.type}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                ></iframe>
+              </VStack>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+      <Box>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y, Controller]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <Flex flexDir={"column"} gap={"20px"}>
+            {similarAllFilm?.map((e) => (
+              <SwiperSlide key={e.id}>
+                <VStack
+                  className="film-card"
+                  spacing={4}
+                  p={4}
+                  border="1px solid"
+                  borderColor="gray.200"
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.3s"
+                  _hover={{
+                    transform: "scale(1.05)",
+                    boxShadow: "lg",
+                    cursor: "pointer",
+                  }}
+                  onClick={() =>
+                    navigate(`/films/${e?.title?.toLowerCase()}-${e?.id}`)
+                  }
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w500${e.poster_path}`}
+                    alt={e.title}
+                    borderRadius="md"
+                  />
+                  <Heading as="h3" size="md" textAlign="center">
+                    {e.title}
+                  </Heading>
+                  <Text
+                    noOfLines={3}
+                    textAlign="center"
+                    fontSize="sm"
+                    color="gray.600"
+                  >
+                    {e.overview}
+                  </Text>
+                </VStack>
+              </SwiperSlide>
+            ))}
+          </Flex>
+        </Swiper>
+      </Box>
+    </>
   );
 }
